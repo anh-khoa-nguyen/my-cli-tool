585d5d974b3d181dd9471ef1bf1caa46d4af5b23
anh-khoa-nguyen <nak.khoanguyen@gmail.com>
2026-05-14 09:54:04 +0700

    push

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-echo "dang tai mycmd tool..."
+echo "mycmd tool..."
 # THAY LINK RAW GITHUB CUA FILE 'mycmd' VAO DUONG LINK DUOI DAY
 curl -sL "https://raw.githubusercontent.com/anh-khoa-nguyen/my-cli-tool/main/mycmd" -o /usr/local/bin/mycmd
 
-echo "dang cap quyen thuc thi..."
+echo "ex..."
 chmod +x /usr/local/bin/mycmd
 
-echo "cai dat thanh cong! bam mycmd de bat dau."
+echo "dn"
